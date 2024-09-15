217974d2d9651d991bd8af0e3121272f776a40ae
Shivanshuk28 <2contactshivanshu@gmail.com>
2024-09-15 13:22:33 +0530

    landing page basic html and js made

---
+++ index.js
@@ -0,0 +1,16 @@
+// Crud for indexedDB.html
+
+// saving the name of player of the game
+let tog = 1;
+let whiteCastleChance = true;
+let blackCastleChance = true;
+document.getElementById('playerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const player1 = document.getElementById('player1').value;
+    const player2 = document.getElementById('player2').value;
+
+    localStorage.setItem('player1', player1);
+    localStorage.setItem('player2', player2);
+
+    window.location.href = 'chess.html';
+});
