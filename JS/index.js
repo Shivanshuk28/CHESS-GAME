5ee7126e7751a7e3d0f7537c8b0a54827940593a
Shivanshuk28 <2contactshivanshu@gmail.com>
2024-09-22 22:19:32 +0530

    more styling

--- JS/index.js
+++ JS/index.js
@@ -4,13 +4,15 @@
 let tog = 1;
 let whiteCastleChance = true;
 let blackCastleChance = true;
-document.getElementById('playerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById('playerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
     const player1 = document.getElementById('player1').value;
     const player2 = document.getElementById('player2').value;
+    const gameTime = document.getElementById('gameTime').value;
 
     localStorage.setItem('player1', player1);
     localStorage.setItem('player2', player2);
+    localStorage.setItem('gameTime', gameTime);
 
-    window.location.href = 'html/chess.html';
+    window.location.href = 'HTML/chess.html';
 });
